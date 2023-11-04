5f30f4702e3e62d3e659bb3dd1bfc4be34c1dbb0
Tensae Girma Seifu <tensae-girm.seifu@chasacademy.se>
2023-11-04 17:58:54 +0100

    Add project files.

---
+++ DbCallUppgift10/Program.cs
@@ -0,0 +1,28 @@
+﻿using DbCallUppgift10.Menu;
+using DbCallUppgift10.Models;
+using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+
+namespace Dbconnectscaffold
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+
+            Menus.MenuItem();
+        }
+    }
+
+
+}
